4f369ad1a5c75b94c6f5568adbcea72df4e33410
Damian Hankins <damianchankins@gmail.com>
2021-04-15 17:14:52 -0400

    netlify upload

--- src/pages/Boosted/Boosted.js
+++ src/pages/Boosted/Boosted.js
@@ -12,7 +12,7 @@ const Boosted = (props) => {
       <h1 style={{ color: "rgba(84, 88, 104, 0.945)" }}>Boosted Boards</h1>
 
       <img src="https://i.imgur.com/KHqZUzmm.jpg" alt="V2" />
-      <button>Review</button>
+      
       <h3 style={{ backgroundColor: "orange" }}>Boosted V2</h3>
       <p>
         Speed: 22mph Range: Standard: 7-8mi. ext.Range: 12-14mi. ChargeTime: 90
